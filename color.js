663c196e48430f314eaaaccf720021873f2c5f79
Harendra Singh <66hari@gmail.com>
2016-04-04 16:11:14 +0530

    Updates color to blue

--- color.js
+++ color.js
@@ -1,7 +1,7 @@
 module.exports = {
   //color: '#038505'  //GREEN
-  //color: '#1657b8'  //BLUE
-  color: '#ac8601'  //ORANGE
+  color: '#1657b8'  //BLUE
+  //color: '#ac8601'  //ORANGE
   //color: '#980c8e'  //PURPLE
   //color: '#bf4912'    //RED
 };
