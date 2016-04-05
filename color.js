cab854b4ee3585729df4ae68a5d4e1c0347332dd
Harendra Singh <66hari@gmail.com>
2016-04-05 13:03:00 +0530

    Updates color to green

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //color: '#038505'  //GREEN
-  color: '#1657b8'  //BLUE
+  color: '#038505'  //GREEN
+  //color: '#1657b8'  //BLUE
   //color: '#ac8601'  //ORANGE
   //color: '#980c8e'  //PURPLE
   //color: '#bf4912'    //RED
