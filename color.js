4df4ac224c82a266d956afcc7dd4ec4ced519ee4
Avi Cavale <avi@shippable.com>
2016-04-19 12:13:11 -0400

    change to green

--- color.js
+++ color.js
@@ -1,8 +1,8 @@
 module.exports = {
-  //color: '#038505'  //GREEN
+  color: '#038505'  //GREEN
   //color: '#1657b8'  //BLUE
   //color: '#ac8601'  //ORANGE
-  color: '#980c8e'  //PURPLE
+  //color: '#980c8e'  //PURPLE
   //color: '#ac8601'  //ORANGE
   //color: '#bf4912'    //RED
 };
