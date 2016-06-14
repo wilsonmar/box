0f376dac84868a5e5216f944874ab869699ae073
Avi Cavale <avi@shippable.com>
2016-06-14 10:05:59 -0700

    adding curl objects

--- curl.sh
+++ curl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
-BODY=$(<./body.json)
-echo "$BODY"
 
-PROJECT_URL='http://localhost:50000/projects/575fa22f71af9ded209ea4f2/newBuild'
-curl -H "Content-Type: application/json" -H "Authorization: apiToken $SHIPPABLE_API_TOKEN" -X POST -d "$BODY" $PROJECT_URL
+BODY=$(<./body.json)
+VERSION_URL='http://beta.shippable.com//v2/versions'
+
+echo "$BODY"
+curl -H "Content-Type: application/json" -H "Authorization: apiToken $SHIPPABLE_API_TOKEN" -X POST -d "$BODY" $VERSION_URL
