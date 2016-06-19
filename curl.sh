0e889c9c7d95bfb95938352c6a24cf01108b1c7a
Avi Cavale <avi@shippable.com>
2016-06-19 10:10:21 -0700

    updating to avinci

--- curl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-BODY=$(<./body.json)
-VERSION_URL='https://apibeta.shippable.com/v2/versions'
-
-echo "$BODY"
-
-curl -v -H "Content-Type: application/json" -H "Authorization: apiToken $SHIPPABLE_API_TOKEN" -X POST "$VERSION_URL" -d "$BODY"
