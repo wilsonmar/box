81f834987ae48e3744b39da3f39665c123439b08
Avi Cavale <avi@shippable.com>
2016-06-19 11:33:01 -0700

    change to blue

--- push.sh
+++ push.sh
@@ -4,9 +4,6 @@ if [ "$IS_PULL_REQUEST" != true ]; then
   echo $TAG
 
   sudo docker push $IMAGE_NAME:$TAG
-  sed -i -e 's/{TAG}/'$TAG'/g' body.json
-
-  cat body.json
 else
   echo "skipping because it's a PR"
 fi
